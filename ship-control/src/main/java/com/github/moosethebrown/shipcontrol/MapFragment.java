955b03306abe02994f4f74d2f02f03a14252ca13
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2025-05-15 18:28:31 +0300

    Implement ship location drawing on the map

--- ship-control/src/main/java/com/github/moosethebrown/shipcontrol/MapFragment.java
+++ ship-control/src/main/java/com/github/moosethebrown/shipcontrol/MapFragment.java
@@ -1,6 +1,7 @@
 package com.github.moosethebrown.shipcontrol;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,18 +14,28 @@ import androidx.lifecycle.ViewModelProviders;
 import com.github.moosethebrown.shipcontrol.data.ShipViewModel;
 import com.github.moosethebrown.shipcontrol.data.Waypoint;
 
-import org.maplibre.android.camera.CameraPosition;
+import org.maplibre.android.camera.CameraUpdateFactory;
 import org.maplibre.android.geometry.LatLng;
 import org.maplibre.android.maps.MapLibreMap;
 import org.maplibre.android.maps.MapView;
+import org.maplibre.android.maps.Style;
+import org.maplibre.android.style.expressions.Expression;
+import org.maplibre.android.style.layers.Property;
+import org.maplibre.android.style.layers.PropertyFactory;
+import org.maplibre.android.style.layers.SymbolLayer;
+import org.maplibre.android.style.sources.GeoJsonSource;
+import org.maplibre.geojson.Feature;
+import org.maplibre.geojson.Point;
 
 public class MapFragment extends Fragment {
     private static final String LOG_TAG = "MapFragment";
+    private static final String SHIP_POSITION_SOURCE = "ship-position-source";
 
     private MapView mapView = null;
     private MapLibreMap map = null;
     private Waypoint shipPosition = null;
-    private boolean cameraSet = false;
+    private double shipAngle = 0.0;
+    private GeoJsonSource shipLocationSource = null;
 
     public MapFragment() {}
 
@@ -41,8 +52,11 @@ public class MapFragment extends Fragment {
         mapView = view.findViewById(R.id.mapView);
         mapView.getMapAsync(map -> {
             this.map = map;
-            map.setStyle("https://tiles.openfreemap.org/styles/liberty");
-            updateCameraPosition();
+            map.setStyle("https://tiles.openfreemap.org/styles/liberty", style -> {
+                initShipLocationSource(style);
+                addShipLocationLayer(style);
+                updateShipPosition();
+            });
         });
 
         return view;
@@ -65,8 +79,19 @@ public class MapFragment extends Fragment {
 
         ShipViewModel viewModel = ViewModelProviders.of(getActivity()).get(ShipViewModel.class);
 
-        setShipPosition(viewModel.getShipPosition().getValue());
-        viewModel.getShipPosition().observe(this, this::setShipPosition);
+        shipPosition = viewModel.getShipPosition().getValue();
+        viewModel.getShipPosition().observe(this, newShipPosition -> {
+            shipPosition = newShipPosition;
+            updateShipPosition();
+        });
+
+        if (viewModel.getAngle().getValue() != null) {
+            shipAngle = viewModel.getAngle().getValue();
+        }
+        viewModel.getAngle().observe(this, newAngle -> {
+            shipAngle = newAngle;
+            updateShipPosition();
+        });
 
         Log.d(LOG_TAG, "onStart()");
     }
@@ -103,26 +128,50 @@ public class MapFragment extends Fragment {
         mapView.onSaveInstanceState(outState);
     }
 
-    private void updateCameraPosition() {
-        if ((map == null) || (shipPosition == null)) {
-            return;
-        }
-        Log.d(LOG_TAG, "updatePosition(), latitude = " +
-                shipPosition.getLatitude() + "; longitude = " +
-                shipPosition.getLongitude());
-        map.setCameraPosition(new CameraPosition.Builder().
-                target(new LatLng(shipPosition.getLatitude(), shipPosition.getLongitude())).
-                zoom(12).build());
+    private void initShipLocationSource(Style style) {
+        shipLocationSource = new GeoJsonSource(SHIP_POSITION_SOURCE);
+        style.addSource(shipLocationSource);
     }
 
-    private void setShipPosition(Waypoint newShipPosition) {
-        if (newShipPosition == null) {
+    private void addShipLocationLayer(Style style) {
+        style.addImage("ship-position-icon",
+                BitmapFactory.decodeResource(getResources(), R.drawable.navigation));
+        SymbolLayer shipLocationLayer = new SymbolLayer("ship-position-layer",
+                SHIP_POSITION_SOURCE).withProperties(PropertyFactory.iconImage("ship-position-icon"),
+                PropertyFactory.iconSize(0.05f),
+                PropertyFactory.iconRotate(Expression.get("rotation")),
+                PropertyFactory.iconAnchor(Property.ICON_ANCHOR_CENTER),
+                PropertyFactory.iconAllowOverlap(true),
+                PropertyFactory.iconIgnorePlacement(true));
+        style.addLayer(shipLocationLayer);
+    }
+
+    private void updateShipPosition() {
+        if (shipPosition == null) {
+            Log.d(LOG_TAG, "updateShipPosition(), shipPosition = null");
             return;
         }
-        shipPosition = newShipPosition;
-        if (!cameraSet) {
-            updateCameraPosition();
-            cameraSet = true;
+        if (map == null) {
+            Log.d(LOG_TAG, "updateShipPosition(), map = null");
+            return;
         }
+
+        Log.d(LOG_TAG, "updateShipPosition(), latitude = " + shipPosition.getLatitude() +
+                ", longitude = " + shipPosition.getLongitude());
+
+        map.getStyle(style -> {
+           Point point = Point.fromLngLat(shipPosition.getLongitude(), shipPosition.getLatitude());
+           Feature feature = Feature.fromGeometry(point);
+
+           feature.addNumberProperty("rotation", shipAngle);
+
+           shipLocationSource = style.getSourceAs(SHIP_POSITION_SOURCE);
+           if (shipLocationSource != null) {
+               shipLocationSource.setGeoJson(feature);
+           }
+
+           map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(shipPosition.getLatitude(),
+                   shipPosition.getLongitude()), 15));
+        });
     }
 }
